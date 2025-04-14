caa07f944e69fd09ba3ccbf8b348b1b77ef62636
Shahab Yarmohammadi <yarmohammadi_shahab@student.smc.edu>
2025-04-13 21:40:10 -0700

    First working version with Google streetview images fetched via API ***Requires API Keys

--- scripts.js
+++ scripts.js
@@ -1,88 +1,182 @@
 /**
- * Data Catalog Project Starter Code - SEA Stage 2
+ * Santa Monica Rent Control Catalog
  *
- * This file is where you should be doing most of your work. You should
- * also make changes to the HTML and CSS files, but we want you to prioritize
- * demonstrating your understanding of data structures, and you'll do that
- * with the JavaScript code you write in this file.
- *
- * The comments in this file are only to help you learn how the starter code
- * works. The instructions for the project are in the README. That said, here
- * are the three things you should do first to learn about the starter code:
- * - 1 - Change something small in index.html or style.css, then reload your
- *    browser and make sure you can see that change.
- * - 2 - On your browser, right click anywhere on the page and select
- *    "Inspect" to open the browser developer tools. Then, go to the "console"
- *    tab in the new window that opened up. This console is where you will see
- *    JavaScript errors and logs, which is extremely helpful for debugging.
- *    (These instructions assume you're using Chrome, opening developer tools
- *    may be different on other browsers. We suggest using Chrome.)
- * - 3 - Add another string to the titles array a few lines down. Reload your
- *    browser and observe what happens. You should see a fourth "card" appear
- *    with the string you added to the array, but a broken image.
- *
+ * The city of Santa Monica's Rent Control Board maintains a public database
+ * of rent controlled properties in the city at 
+ * https://www.smgov.net/departments/rentcontrol/mar.aspx
+ * 
+ * The interface is very limited and allows for looking up units in an
+ * individual building with an interface that leaves much to be desired.
+ * 
+ * I have scraped the data using a python script and stored it in a CSV
+ * datased which is stored locally in the root of this project.
+ * 
+ * This project is aimed at bringing the data to life and allowing for
+ * better visualization, and more interaction with the data.
+ * 
  */
 
-const FRESH_PRINCE_URL =
-  "https://upload.wikimedia.org/wikipedia/en/3/33/Fresh_Prince_S1_DVD.jpg";
-const CURB_POSTER_URL =
-  "https://m.media-amazon.com/images/M/MV5BZDY1ZGM4OGItMWMyNS00MDAyLWE2Y2MtZTFhMTU0MGI5ZDFlXkEyXkFqcGdeQXVyMDc5ODIzMw@@._V1_FMjpg_UX1000_.jpg";
-const EAST_LOS_HIGH_POSTER_URL =
-  "https://static.wikia.nocookie.net/hulu/images/6/64/East_Los_High.jpg";
 
-// This is an array of strings (TV show titles)
-let titles = [
-  "Fresh Prince of Bel Air",
-  "Curb Your Enthusiasm",
-  "East Los High",
-];
-// Your final submission should have much more data than this, and
-// you should use more than just an array of strings to store it all.
+let units = []; // Placeholder array for the parsed dataset of units
+let currentPage = 1; // Defaulting current page to 1
+const cardsPerPage = 10; // Number of cards to display per page
 
-// This function adds cards the page to display the data in the array
-function showCards() {
+// WARNING!!! Storing your API key here will expose it to viewers
+// If you are not using locally you must restrict domain, referer, and set limits
+const apiKey = "YOUR_API_KEY_GOES_HERE"; // Enter your Google Maps API key here. 
+
+fetch('units.csv')
+    .then(response => response.text())
+    .then(data => {
+        // Process the CSV data
+        units = parseCSV(data);
+        //console.log(units);
+        shuffleCards(); // Shuffle the units initially
+        // Calls showCards() func once data is fetched and parsed
+        showCards(currentPage);
+    })
+    .catch(error => {
+        console.error('An error occured while fetching the CSV file:', error);
+    });
+
+function parseCSV(csv) {
+  const lines = csv.split('\n'); // Split each line of data
+  const result = []; // Array to store dataset
+  const headers = lines[0].split(',').map(header => header.trim()); // First row aka the header to use as keys
+
+  // Loop through each line after header
+  for (let i = 1; i < lines.length; i++) {
+    const obj = {}; // Object to hold contents of each row/line
+    let currentline = lines[i];
+    const values = []; // Array to hold values
+    let inQuotes = false; // Is value in quotation marks?
+    let currentValue = '';
+
+    // Escaping quotation marks
+    for (let char of currentline) {
+        if (char === '"') {
+            // Flip quotation mark flag
+            inQuotes = !inQuotes;
+        } else if (char === ',' && !inQuotes) {
+            // End of field, push value to array
+            values.push(currentValue.trim());
+            currentValue = '';
+        } else {
+            // Read next character
+            currentValue += char;
+        }
+      }
+
+    // Push the last value
+    values.push(currentValue.trim());
+    
+    // Loop thorugh values of row
+    for (let j = 0; j < headers.length; j++) {
+      // Replace leading and/or trailing double quotes
+      const value = values[j] ? values[j].replace(/^"|"$/g, '').trim() : '';
+      // Update object value
+      obj[headers[j]] = value;
+    }
+
+    // Push new row object to array
+    result.push(obj);
+  }
+
+  return result;
+}
+
+const Home_Image_URL = "./pexels-binyaminmellish-106399.jpg";
+
+
+// This function adds cards to the page to display the data in the array
+function showCards(page) {
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
-  for (let i = 0; i < titles.length; i++) {
-    let title = titles[i];
+  const start = (page - 1) * cardsPerPage;
+  const end = start + cardsPerPage;
+  const paginatedUnits = units.slice(start, end);
 
-    // This part of the code doesn't scale very well! After you add your
-    // own data, you'll need to do something totally different here.
-    let imageURL = "";
-    if (i == 0) {
-      imageURL = FRESH_PRINCE_URL;
-    } else if (i == 1) {
-      imageURL = CURB_POSTER_URL;
-    } else if (i == 2) {
-      imageURL = EAST_LOS_HIGH_POSTER_URL;
-    }
+  for (let i = 0; i < paginatedUnits.length; i++) {
+    let unit = paginatedUnits[i];
+    let cardID = sanitizeId(unit["Address"]+unit["Unit"])
+
+    let imageURL = Home_Image_URL;
+    // Get Streetview Image from Google Maps
+    getLatLng(unit["Address"] + ', Santa Monica, CA')
+    .then(latLng => {
+      //streetViewUrl
+      const imageURL = `https://maps.googleapis.com/maps/api/streetview?size=600x400&location=${latLng}&key=${apiKey}`;
+      document.getElementById(cardID).src = imageURL;
+    })
+    .catch(error => {
+      console.error(error);
+    });
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
-    editCardContent(nextCard, title, imageURL); // Edit title and image
+    editCardContent(nextCard
+      , unit["Address"] + (unit["Unit"] ? ' #' + unit["Unit"] : '')
+      , imageURL, unit["MAR"], cardID); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
+
+  updatePaginationControls();
 }
 
-function editCardContent(card, newTitle, newImageURL) {
+// Func to update pagination controls
+function updatePaginationControls() {
+  const totalPages = Math.ceil(units.length / cardsPerPage);
+  document.getElementById("prev-page").disabled = currentPage === 1;
+  document.getElementById("next-page").disabled = currentPage === totalPages;
+}
+
+// Load these last to avoid not picking up the elements before rendering
+document.addEventListener("DOMContentLoaded", function() {
+
+  // Event listeners for pagination controls
+  document.getElementById("prev-page").addEventListener("click", () => {
+    if (currentPage > 1) {
+      currentPage--;
+      showCards(currentPage);
+    }
+  });
+
+  document.getElementById("next-page").addEventListener("click", () => {
+    const totalPages = Math.ceil(units.length / cardsPerPage);
+    if (currentPage < totalPages) {
+      currentPage++;
+      showCards(currentPage);
+    }
+  });
+});
+
+function editCardContent(card, newTitle, newImageURL, newPrice, newID) {
   card.style.display = "block";
 
-  const cardHeader = card.querySelector("h2");
+  const cardHeader = card.querySelector("b");
   cardHeader.textContent = newTitle;
+
+  const cardPrice = card.querySelector("i");
+  cardPrice.textContent = newPrice;
 
   const cardImage = card.querySelector("img");
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Poster";
+  cardImage.id = newID;
 
-  // You can use console.log to help you debug!
-  // View the output by right clicking on your website,
-  // select "Inspect", then click on the "Console" tab
-  console.log("new card:", newTitle, "- html: ", card);
 }
 
 // This calls the addCards() function when the page is first loaded
-document.addEventListener("DOMContentLoaded", showCards);
+//document.addEventListener("DOMContentLoaded", showCards);
+
+function shuffleCards() {
+  units = units.filter(unit => unit.Bedrooms <= 1);
+  shuffleArray(units);
+  //console.log(units);
+  //units.pop(); // Remove last unit in units array
+  showCards(currentPage); // Call showCards again to refresh
+}
 
 function quoteAlert() {
   console.log("Button Clicked!");
@@ -91,7 +185,27 @@ function quoteAlert() {
   );
 }
 
-function removeLastCard() {
-  titles.pop(); // Remove last item in titles array
-  showCards(); // Call showCards again to refresh
+// Fisher-Yates algorithm to shuffle an array randomly
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]]; // Swap elements
+  }
+  return array;
+}
+
+async function getLatLng(address) {
+  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`);
+  const data = await response.json();
+  if (data.results && data.results[0]) {
+    const location = data.results[0].geometry.location;
+    return `${location.lat},${location.lng}`;
+  } else {
+    throw new Error('Address not found');
+  }
+}
+
+// Func to Replace invalid HTML element id characters with a hyphen
+function sanitizeId(arg) {
+  return arg.replace(/[^a-zA-Z0-9-_:\.]/g, '-');
 }
