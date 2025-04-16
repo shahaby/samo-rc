1c65d9f5e6133c162c3a49385e99a48cc93b061e
Shahab Yarmohammadi <yarmohammadi_shahab@student.smc.edu>
2025-04-15 22:00:52 -0700

    Update parseCSV func to recognize data in double quotes Update CSS to make cards stack in rows Trimmed Dataset and removed Parcel information Add an intro comment to the heading of the JS file Add unit # to the title of the card unless if it's missing Add a home photo feom Pexel to make the visuals more relevant Add pagination with next and prev Add feeling lucky button Add I'd to each card and wrote sanitize id func Add Google geocode and street view apis
    Apr 13:
    Stripped API Key
    Committed and pushed the first working version w/ Google Streetview Image
    PHP microservice to fetch streetview images
    
    Apr 14:
    Add search
    Add toggle buttons for bedrooms
    Add price range
    Add numbered pages for pagination
    Add number of units
    
    Apr 15:
    Add Number of cards per page
    Add sorting (Beds, price, ...)
    Refined Tenancy Date appearance and renamed column
    Add ellipsis to pagination
    Add all data nicely to the cards and remove irrelevant text
    Add saved units feature
    Add link to Google Maps
    Add favicon
    Add comments
    Fully Tested Locally

--- README.md
+++ README.md
@@ -1,132 +1,47 @@
 # SEA Stage 2 - Data Catalog Project
 
-This repository contains the instructions, requirements, and starter code for Stage Two of the Snap Engineering Academy application process.
+This repository contains the project I'm submitting for Stage Two of the Snap Engineering Academy 2025 application process.
 
 ---
 
-## 📚 Table of Contents
+## 🎯 The Project
 
-- [Your Task](#your-task)
-- [Requirements](#requirements---your-catalog-website-should)
-- [Getting Started](#getting-started)
-- [Submitting](#submitting)
-- [❓ Frequently Asked Questions (FAQ)](#-frequently-asked-questions-faq)
+### <p align="center">Santa Monica Rent Control Database</p>
 
----
-
-## 🎯 Your Task
-
-### <p align="center">Create a "catalog" website for something you're passionate about.</p>
-
-> 📝 **Note:**  
-> If you have not used GitHub or programmed a website with JavaScript before, that's OK! Part of the challenge is figuring out things you're not familiar with.
+> 📝 **Background**  
+> The city of Santa Monica's Rent Control Board maintains a publicly accessible database of rent controlled properties in the city at https://www.smgov.net/departments/rentcontrol/mar.aspx
+>
+> The interface is very limited and only allows for looking up units in an individual building with an interface that leaves much to be desired.
+>
+> I have scraped the data using a python script and stored it in a CSV
+> dataset which is stored locally in the root of this project.
+>
+> This project is aimed at bringing the data to life and allowing for better visualization, and more interaction with the data.
 
 ---
 
-## ✅ Requirements - Your Catalog Website Should...
+## ✅ Features
 
-- Show off your understanding of basic data structures: **arrays** and **objects**.
-- Display a **substantial amount of interesting data** in a "catalog". You can look online for datasets or create your own.  
-  Your data should be easy to find in your source code—either in variables at the top of `scripts.js` or imported from a file.
-- Include **two or more features** that operate on your data and modify how it's displayed. Examples:
+- Data structures used: **arrays** and **objects**.
+- Displays a **substantial amount of interesting data** in a "catalog".
+  The data is imported from a file (units.csv) and parsed and pushed to an array of objects.
+- Features include but are not limited to:
   - Filtering
   - Searching
   - Sorting
   - Updating
-  - Adding/removing entries  
-    Think about what users might want to do while using your site!
-- Look polished ✨. Use **HTML** and **CSS** to make your data easy to read and visually appealing.
-- Be built from this **starter code**—you can change anything you want, but build on top of it.
-- Be an **original** project. Please do not submit something previously created for a class, internship, or client.  
-  You are encouraged to use online resources, but **make sure you understand every line of code** in your project.
+  - Adding/removing entries
 
 ---
 
-## 🛠️ Getting Started
+## 🚀 Live Version
 
-1. **Create a GitHub account** if you haven't already.
-2. Click the green **"Use as Template"** button in the upper right corner, then choose **"Create a new repository"**:  
-   a. Choose yourself as the owner  
-   b. Give your repository a name  
-   c. Click **"Create Repository"**
-3. Copy or download the files to your own computer.
-4. Open and edit the website using a text editor:
-   - Modify `index.html`, `style.css`, and `scripts.js`.
-   - To preview, open `index.html` in a web browser (double-click it).
-   - You should see something like this:
+1. **A live version of this repo is published to and can be viewed at:**  
+   https://yarmohammadi.com/samo-rc/
+2. **It has been tested and works with the following:**  
+   Chrome browser on Mac OS
+3. **A screenshot of the live website can be viewed below👇🏻**
 
-<img height="300" alt="Screenshot of catalog example" src="https://github.com/Snap-Engineering-Academy-2023/rn_lab1/assets/7607483/fdd57236-50fe-48ca-956d-d9b4b12db038">
+<img height="300" alt="Screenshot of catalog example" src="https://yarmohammadi.com/samo-rc/SaMo-RC-screenshot.png">
 
 ---
-
-## 🚀 Submitting
-
-1. **Publish your website to the internet!**  
-   We recommend using [GitHub Pages](https://docs.github.com/en/pages/getting-started-with-github-pages/creating-a-github-pages-site#creating-your-site).
-2. **Test the published version.**  
-   Make sure everything works properly before submitting.
-3. **Update your GitHub repository** so it reflects the latest version of your project.
-4. **Submit** both:
-   - The **URL** to your published website
-   - The **link** to your GitHub repository  
-     …via the Google Form linked in your email.
-
----
-
-## ❓ Frequently Asked Questions (FAQ)
-
-### ❄️ Is it OK that my catalog resets when I refresh the page?
-
-**Yes!** That's exactly what the starter code does too. You don't need to worry about preserving data after a page refresh.
-
----
-
-### 💻 Can I copy bits of code from online resources?
-
-**Yes, absolutely!** You should search for and use **small chunks** of code.  
-For example:
-
-- ✅ Copying code to create a dropdown menu is fine.
-- ❌ Copying a full “filter data by date” feature is not.
-
-Be thoughtful about what you borrow!
-
----
-
-### 🤖 Can I use generative AI (like ChatGPT or Copilot)?
-
-**Partially.**
-
-- ✅ You _may_ use AI tools to help write **HTML** and **CSS**.
-- ❌ You _may not_ use AI to write **JavaScript**.
-
-HTML/CSS can be tricky to get right, and it's okay to get help making things look nice.  
-But JavaScript is where you show your own logic and understanding of data!
-
----
-
-### 🧰 Can I use a different template?
-
-**Nope!**  
-You must use the provided starter code, though you're free to customize it however you like.  
-You _can_ copy **small chunks** from other templates if needed.
-
----
-
-### 🛠️ Can I use a framework like React, Vue, Bootstrap, or Tailwind?
-
-**Nope!**  
-This project is for folks new to web dev. Frameworks do a lot of heavy lifting and hide the logic we're asking you to demonstrate.  
-SEA will teach you frameworks later—stick to **vanilla HTML, CSS, and JS** for now.
-
----
-
-### 🌐 Can I use APIs?
-
-**Nope!**  
-APIs can add complexity beyond the scope of this challenge.  
-If you really want to use data from an API, **save it to a file** (like `.json` or `.csv`) and use that instead.
-
----
-
-> 💬 **Have any questions?** Drop them on the [Padlet here](https://padlet.com/arlenschallenge/2025-snap-engineering-academy-stage-2-project-assessment-que-ndmqkef3wxt8sh72) and we'll get back to you!
--- scripts.js
+++ scripts.js
@@ -1,48 +1,47 @@
 /**
  * Santa Monica Rent Control Catalog
  *
- * The city of Santa Monica's Rent Control Board maintains a public database
- * of rent controlled properties in the city at 
+ * The city of Santa Monica's Rent Control Board maintains a publicly accessible
+ * database of rent controlled properties in the city at 
  * https://www.smgov.net/departments/rentcontrol/mar.aspx
  * 
- * The interface is very limited and allows for looking up units in an
+ * The interface is very limited and only allows for looking up units in an
  * individual building with an interface that leaves much to be desired.
  * 
  * I have scraped the data using a python script and stored it in a CSV
- * datased which is stored locally in the root of this project.
+ * dataset which is stored locally in the root of this project.
  * 
  * This project is aimed at bringing the data to life and allowing for
  * better visualization, and more interaction with the data.
  * 
- */
+ **/
 
-
-let units = []; // Placeholder array for the parsed dataset of units
+let units = []; // Placeholder array for parsed dataset of units
+let cards = []; // Placeholder array for filtered units
 let currentPage = 1; // Defaulting current page to 1
-const cardsPerPage = 10; // Number of cards to display per page
-
-// WARNING!!! Storing your API key here will expose it to viewers
-// If you are not using locally you must restrict domain, referer, and set limits
-const apiKey = "YOUR_API_KEY_GOES_HERE"; // Enter your Google Maps API key here. 
+let cardsPerPage = 10; // Number of cards to display per page
+let selectedBeds = []; // Placeholder array for filtering based on beds
+let faveUnits = []; // Placeholder array for storing fave units
+const Home_Image_URL = "./pexels-binyaminmellish-106399.jpg";
+// const savedButt = document.getElementById("saved-units");
 
 fetch('units.csv')
     .then(response => response.text())
     .then(data => {
-        // Process the CSV data
-        units = parseCSV(data);
-        //console.log(units);
-        shuffleCards(); // Shuffle the units initially
-        // Calls showCards() func once data is fetched and parsed
-        showCards(currentPage);
+        units = parseCSV(data); // Process the CSV data
+        cards = units; // Copy to cards for filtering
+        //console.log(units.length);
+        shuffleCards(); // Shuffle the units initially for better ux
+        showCards(currentPage); // Show cards once data is fetched and parsed
     })
     .catch(error => {
         console.error('An error occured while fetching the CSV file:', error);
     });
 
 function parseCSV(csv) {
-  const lines = csv.split('\n'); // Split each line of data
+  const lines = csv.split('\n'); // Split each line
   const result = []; // Array to store dataset
-  const headers = lines[0].split(',').map(header => header.trim()); // First row aka the header to use as keys
+  const headers = lines[0].split(','); // First row aka the header to use as keys
 
   // Loop through each line after header
   for (let i = 1; i < lines.length; i++) {
@@ -70,11 +69,9 @@ function parseCSV(csv) {
     // Push the last value
     values.push(currentValue.trim());
     
-    // Loop thorugh values of row
+    // Loop fields in row and update obj keys
     for (let j = 0; j < headers.length; j++) {
-      // Replace leading and/or trailing double quotes
-      const value = values[j] ? values[j].replace(/^"|"$/g, '').trim() : '';
-      // Update object value
+      const value = values[j];
       obj[headers[j]] = value;
     }
 
@@ -85,39 +82,35 @@ function parseCSV(csv) {
   return result;
 }
 
-const Home_Image_URL = "./pexels-binyaminmellish-106399.jpg";
 
-
-// This function adds cards to the page to display the data in the array
+// Func to display cards
 function showCards(page) {
+  cardsPerPage = parseInt(document.getElementById("units-per-page")?.value) || cardsPerPage;
+
+  const unitsCount = document.getElementById("units-count");
+  unitsCount.textContent = addCommas(cards.length);
+
   const cardContainer = document.getElementById("card-container");
   cardContainer.innerHTML = "";
   const templateCard = document.querySelector(".card");
 
   const start = (page - 1) * cardsPerPage;
   const end = start + cardsPerPage;
-  const paginatedUnits = units.slice(start, end);
+  const paginatedCards = cards.slice(start, end);
+  // console.log("start = " + start + " | end = " + end + " | cardsPerPage = " + cardsPerPage);
 
-  for (let i = 0; i < paginatedUnits.length; i++) {
-    let unit = paginatedUnits[i];
-    let cardID = sanitizeId(unit["Address"]+unit["Unit"])
+  for (let i = 0; i < paginatedCards.length; i++) {
+    let unit = paginatedCards[i];
+    let cardID = sanitizeId(unit["Address"]+unit["Unit"]) // Element ID for the card image
 
     let imageURL = Home_Image_URL;
-    // Get Streetview Image from Google Maps
-    getLatLng(unit["Address"] + ', Santa Monica, CA')
-    .then(latLng => {
-      //streetViewUrl
-      const imageURL = `https://maps.googleapis.com/maps/api/streetview?size=600x400&location=${latLng}&key=${apiKey}`;
-      document.getElementById(cardID).src = imageURL;
-    })
-    .catch(error => {
-      console.error(error);
-    });
+    imageURL = 'https://yarmohammadi.com/samo-rc/streetview/?address=' + unit["Address"] + ', Santa Monica, CA'
 
     const nextCard = templateCard.cloneNode(true); // Copy the template card
     editCardContent(nextCard
       , unit["Address"] + (unit["Unit"] ? ' #' + unit["Unit"] : '')
-      , imageURL, unit["MAR"], cardID); // Edit title and image
+      , imageURL, unit["MAR"], unit["Bedrooms"], unit["Date"]
+      , cardID); // Edit title and image
     cardContainer.appendChild(nextCard); // Add new card to the container
   }
 
@@ -126,63 +119,204 @@ function showCards(page) {
 
 // Func to update pagination controls
 function updatePaginationControls() {
-  const totalPages = Math.ceil(units.length / cardsPerPage);
-  document.getElementById("prev-page").disabled = currentPage === 1;
-  document.getElementById("next-page").disabled = currentPage === totalPages;
+  const totalPages = Math.ceil(cards.length / cardsPerPage);
+  document.getElementById("prev-page").disabled = currentPage == 1;
+  document.getElementById("next-page").disabled = currentPage == totalPages;
+
+  const pageNumbersContainer = document.getElementById("page-numbers");
+    pageNumbersContainer.innerHTML = ""; // Clear existing page numbers
+
+    for (let i = 1; i <= totalPages; i++) {
+      if (i == 1 || i == totalPages || (i <= currentPage + 3 && i >= currentPage - 3)){
+        const pageNumber = document.createElement("span");
+        pageNumber.textContent = addCommas(i);
+        pageNumber.classList.add("page-number");
+        pageNumber.style.margin = "0 5px"; // Add space
+
+        if (i != currentPage){
+          pageNumber.style.cursor = "pointer";
+          // Add click event to each page no
+          pageNumber.addEventListener("click", () => {
+              currentPage = i;
+              showCards(currentPage);
+          });
+        }
+
+        pageNumbersContainer.appendChild(pageNumber);
+      } else if (i == currentPage + 5 || i == currentPage - 5)
+      {
+        const pageNumber = document.createElement("span");
+        pageNumber.textContent = "...";
+        pageNumbersContainer.appendChild(pageNumber);
+      }
+    }
 }
 
-// Load these last to avoid not picking up the elements before rendering
-document.addEventListener("DOMContentLoaded", function() {
+// prevPage butt onClick Listener
+function prevPage(){
+  if (currentPage > 1) {
+    currentPage--;
+    showCards(currentPage);
+    console.log(currentPage);
+  }
+}
 
-  // Event listeners for pagination controls
-  document.getElementById("prev-page").addEventListener("click", () => {
-    if (currentPage > 1) {
-      currentPage--;
-      showCards(currentPage);
-    }
-  });
+// nextPage butt onClick Listener
+function nextPage(){
+  const totalPages = Math.ceil(cards.length / cardsPerPage);
+  if (currentPage < totalPages) {
+    currentPage++;
+    showCards(currentPage);
+    console.log(currentPage);
+  }
+}
 
-  document.getElementById("next-page").addEventListener("click", () => {
-    const totalPages = Math.ceil(units.length / cardsPerPage);
-    if (currentPage < totalPages) {
-      currentPage++;
-      showCards(currentPage);
-    }
-  });
-});
-
-function editCardContent(card, newTitle, newImageURL, newPrice, newID) {
+// Func to template new cards
+function editCardContent(card, newTitle, newImageURL, newPrice, newBeds, newDate, newID) {
   card.style.display = "block";
 
-  const cardHeader = card.querySelector("b");
+  const cardHeader = card.querySelector("h2");
   cardHeader.textContent = newTitle;
 
-  const cardPrice = card.querySelector("i");
-  cardPrice.textContent = newPrice;
+  const bullets = card.querySelectorAll("li");
+  bullets[0].textContent = '$' + addCommas(newPrice) + ".00 | ";
+  bullets[1].textContent = newBeds == 0 ? "Studio" : newBeds + " beds";
+  newDate = newDate ? " | " + new Date(newDate).toDateString().slice(4) : ""
+  bullets[2].textContent = newDate;
 
   const cardImage = card.querySelector("img");
   cardImage.src = newImageURL;
   cardImage.alt = newTitle + " Poster";
   cardImage.id = newID;
 
+  const cardMapLink = card.querySelector("a");
+  cardMapLink.href = "https://maps.google.com/?q=" + encodeURIComponent(newTitle + ", Santa Monica, CA");
+  
+  const favIcon = card.querySelector("span");
+  //pageNumber.style.cursor = "pointer";
+  if (faveUnits.includes(newTitle)){
+    favIcon.classList.add("selected");
+  }
+  // Add click event to each star icon
+  favIcon.addEventListener("click", () => {
+      //this.classList.toggle('selected');
+      if (faveUnits.includes(newTitle)){
+        faveUnits = faveUnits.filter(item=>item!=newTitle);
+        if (document.getElementById("saved-units").classList.contains("on")){
+          card.remove();
+          //Add code to update no. of units after card removal
+        }
+      } else {
+        faveUnits.push(newTitle);
+      }
+      console.log(faveUnits);
+      document.getElementById('saved-units').textContent = "★ Saved (" + faveUnits.length + ")";
+  });
+
 }
 
-// This calls the addCards() function when the page is first loaded
-//document.addEventListener("DOMContentLoaded", showCards);
+// Func to reset filter controls
+function resetCards() {
+  document.getElementById("search-query").value = '';
+  document.getElementById("min-price").value = '';
+  document.getElementById("max-price").value = '';
 
+  selectedBeds.length = 0;
+  for (let i=0;i<=4;i++){
+    const myButton = document.getElementById("toggle" + i);
+    myButton.classList.remove("on");
+    myButton.classList.add("off");
+  }
+  const myButton = document.getElementById("saved-units");
+  myButton.classList.remove("on")
+
+  filterCards();
+}
+
+// Func to filter cards array based on active filter controls
+function filterCards() {
+  const showSaved = document.getElementById("saved-units").classList.contains("on");
+    let minPrice = parseInt(document.getElementById("min-price").value) || 0;
+    let maxPrice = parseInt(document.getElementById("max-price").value) || 9999999;
+    maxPrice = maxPrice > minPrice ? maxPrice : 9999999; // Set maxPrice to 9999999 if it's not greater than minPrice for better UX
+    let searchQuery = document.getElementById("search-query").value.toUpperCase();
+
+    //console.log(`search query = ${searchQuery}
+    //   | min = ${minPrice} | max = ${maxPrice} | beds = ${selectedBeds}`);
+    
+    cards = units.filter(unit => parseInt(unit.MAR) > minPrice 
+            && parseInt(unit.MAR) < parseFloat(maxPrice) 
+            && unit.Address.includes(searchQuery)
+            && (selectedBeds.length === 0 || selectedBeds.some(bed => {
+              if (bed == 4) // Special condition for 4+ button
+              {
+                return unit.Bedrooms >= 4;
+              }
+              return unit.Bedrooms == bed;
+            }))
+            && (showSaved === false || faveUnits.length === 0 || faveUnits.includes(unit.Address + (unit.Unit ? ' #' + unit.Unit : '')))
+          );
+
+  // Sort the array based on user selection
+  const sortby = document.getElementById("sort-cards").value;
+  switch (sortby) {
+    case "price-asc":
+      cards = cards.sort((a,b) => a.MAR - b.MAR);
+      break;
+    case "price-desc":
+      cards = cards.sort((a,b) => b.MAR - a.MAR);
+      break;
+    case "beds-asc":
+      cards = cards.sort((a,b) => a.Bedrooms - b.Bedrooms);
+      break;
+    case "beds-desc":
+      cards = cards.sort((a,b) => b.Bedrooms - a.Bedrooms);
+      break;
+    case "date-asc":
+      cards = cards.sort((a,b) => 
+        (a.Date ? new Date(a.Date) : new Date('1/1/1800')) - (b.Date ? new Date(b.Date) : new Date('1/1/1800')));
+      break;
+    case "date-desc":
+      cards = cards.sort((a,b) => 
+        (b.Date ? new Date(b.Date) : new Date('1/1/1800')) - (a.Date ? new Date(a.Date) : new Date('1/1/1800')));
+      break;
+    default:
+      // Don't sort!
+  }
+
+  const totalPages = Math.ceil(cards.length / cardsPerPage);
+  showCards(currentPage <= totalPages ? currentPage : totalPages);
+}
+
+// Toggle saved units butt
+function toggleSaved() {
+  const myButton = document.getElementById("saved-units");
+  myButton.classList.toggle('on');
+  filterCards();
+}
+
+// Toggle bed butts
+function toggleBeds(arg) {
+  const myButton = document.getElementById("toggle" + arg);
+    
+  // Toggle the class and text based on the current state
+  if (myButton.classList.contains("off")) {
+    myButton.classList.remove("off");
+    myButton.classList.add("on");
+    selectedBeds.push(arg);
+  } else {
+    myButton.classList.remove("on");
+    myButton.classList.add("off");
+    selectedBeds = selectedBeds.filter(item => item != arg);
+  }
+
+  filterCards();
+}
+
+// Shuffle cards randomly
 function shuffleCards() {
-  units = units.filter(unit => unit.Bedrooms <= 1);
-  shuffleArray(units);
-  //console.log(units);
-  //units.pop(); // Remove last unit in units array
+  shuffleArray(cards);
   showCards(currentPage); // Call showCards again to refresh
-}
-
-function quoteAlert() {
-  console.log("Button Clicked!");
-  alert(
-    "I guess I can kiss heaven goodbye, because it got to be a sin to look this good!"
-  );
 }
 
 // Fisher-Yates algorithm to shuffle an array randomly
@@ -194,18 +328,18 @@ function shuffleArray(array) {
   return array;
 }
 
-async function getLatLng(address) {
-  const response = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`);
-  const data = await response.json();
-  if (data.results && data.results[0]) {
-    const location = data.results[0].geometry.location;
-    return `${location.lat},${location.lng}`;
-  } else {
-    throw new Error('Address not found');
-  }
-}
-
 // Func to Replace invalid HTML element id characters with a hyphen
 function sanitizeId(arg) {
   return arg.replace(/[^a-zA-Z0-9-_:\.]/g, '-');
+}
+
+// Func to add commas w/ regex for better readability
+function addCommas(arg){
+  return String(arg).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
+
+function disclaimerAlert() {
+  alert(
+    "Disclaimer: This web app is for informational purposes only. The information provided is not guaranteed to be accurate, complete, or up-to-date. Use at your own risk. We are not responsible for any errors or omissions, or for any outcomes related to the use of this app. By using this app, you agree to these terms."
+  );
 }
